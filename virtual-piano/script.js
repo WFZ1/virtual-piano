ae0646dab69af92c0133c49e8e4666d6952d19b7
WFZ1 <pavelskumenko@gmail.com>
2021-03-17 14:39:19 +0200

    feat: play sound when move the cursor while holding the left mouse button over the piano keys

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,17 +1,50 @@
 (function () {
   const piano = document.querySelector('.piano'),
         pianoКeys = document.querySelectorAll('.piano-key');
-  
-  /* PLAY SOUND PIANO KEY ============================================================ */
         
   function playAudio (src) {
-    const audio = new Audio();
-    audio.src = src;
+    const audio = new Audio(src);
     audio.currentTime = 0;
     audio.play();
   }
 
-  /* ACTIVATE PIANO KEY ============================================================ */
+  /* MOUSE EVENT ============================================================ */
+
+  const startSound = e => {
+    if ( e.target.classList.contains('piano-key') ) {
+      e.target.classList.add('active');
+      playAudio(`assets/audio/${ e.target.dataset.note }.mp3`);
+    }
+  }
+
+  const deactiveKey = e => {
+    if ( e.target.classList.contains('piano-key') ) {
+      e.target.classList.remove('active');
+    }
+  }
+
+  const startCorrespondOver = e => {
+    startSound(e);
+
+    pianoКeys.forEach(el => {
+      el.addEventListener('mouseover', startSound);
+      el.addEventListener('mouseout', deactiveKey);
+    });
+  }
+
+  const stopCorrespondOver = e => {
+    deactiveKey(e);
+
+    pianoКeys.forEach(el => {
+      el.removeEventListener('mouseover', startSound);
+      el.removeEventListener('mouseout', deactiveKey);
+    });
+  }
+
+  piano.addEventListener('mousedown', startCorrespondOver);
+  piano.addEventListener('mouseup', stopCorrespondOver);
+
+  /* KEYBOARD EVENT ============================================================ */
 
   function activeKey (key) {
     pianoКeys.forEach(el => {
@@ -23,33 +56,18 @@
     key.classList.add('active');
   };
 
-  /* MOUSE CLICK ============================================================ */
-
-  piano.addEventListener('click', (e) => {
-    if (e.target.classList.contains('piano-key')) {
-      activeKey(e.target);
-
-      const note = e.target.dataset.note,
-            src = `assets/audio/${note}.mp3`;
-
-      playAudio(src);
-    }   
-  });
-
-  /* KEYBOARD KEY CLICK ============================================================ */
-
   let letters = {};
 
-  for (key of pianoКeys) {
+  pianoКeys.forEach(key => {
     if ( key.dataset.letter ) {
       letters[key.dataset.letter] = {
         note: key.dataset.note,
         key
       };
     }
-  }
+  });
 
-  window.addEventListener('keydown', (e) => {
+  window.addEventListener('keydown', e => {
     const code = e.code.replace('Key', '');
 
     if ( letters[code] ) {

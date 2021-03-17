c93fbd242abe7f8b5aed7b36c48639d4acab3fa6
WFZ1 <pavelskumenko@gmail.com>
2021-03-17 15:04:42 +0200

    feat: add the toggle of view notes/letters

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,8 +1,19 @@
 (function () {
   const piano = document.querySelector('.piano'),
         pianoКeys = document.querySelectorAll('.piano-key');
-        
-  function playAudio (src) {
+
+  let letters = {};
+
+  pianoКeys.forEach(key => {
+    if ( key.dataset.letter ) {
+      letters[key.dataset.letter] = {
+        note: key.dataset.note,
+        key
+      };
+    }
+  });
+
+  const playAudio = src => {
     const audio = new Audio(src);
     audio.currentTime = 0;
     audio.play();
@@ -46,7 +57,7 @@
 
   /* KEYBOARD EVENT ============================================================ */
 
-  function activeKey (key) {
+  const activeKey = key => {
     pianoКeys.forEach(el => {
       if ( el.classList.contains('active') ) {
         el.classList.remove('active');
@@ -56,23 +67,30 @@
     key.classList.add('active');
   };
 
-  let letters = {};
-
-  pianoКeys.forEach(key => {
-    if ( key.dataset.letter ) {
-      letters[key.dataset.letter] = {
-        note: key.dataset.note,
-        key
-      };
-    }
-  });
-
   window.addEventListener('keydown', e => {
     const code = e.code.replace('Key', '');
 
     if ( letters[code] ) {
       activeKey(letters[code].key);
       playAudio(`assets/audio/${ letters[code].note }.mp3`);
+    }
+  });
+
+  /* VIEW NOTES/LETTERS ============================================================ */
+
+  const btns = document.querySelector('.btn-container');
+
+  btns.addEventListener('click', e => {
+    if (e.target.classList.contains('btn-active')) return;
+
+    e.target.classList.add('btn-active');
+
+    if (e.target.classList.contains('btn-notes')) {
+      e.target.nextElementSibling.classList.remove('btn-active');
+      pianoКeys.forEach(el => el.classList.remove('piano-key-letter'));
+    } else {
+      e.target.previousElementSibling.classList.remove('btn-active');
+      pianoКeys.forEach(el => el.classList.add('piano-key-letter'));
     }
   });
 }());
